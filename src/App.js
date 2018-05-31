e683f39c4cf2225fe7e886f367b43b96d6419289
dzharkov <dmytro.zharkov@zalando.de>
2018-05-31 16:28:17 +0200

    Add styles

--- src/App.js
+++ src/App.js
@@ -2,17 +2,10 @@ import React, { Component } from 'react';
 import ReactDOM from "react-dom";
 import logo from './logo.svg';
 import './App.css';
+import {products} from './mocks/products';
 
 class App extends Component {
-  product = {
-    name: "Langarmshirt - black/legion green/reflective orange",
-    image: "https://mosaic01.ztat.net/vgs/media/catalog-sm/TP/72/1D/0D/FJ/11/TP721D0DF-J11@10.jpg",
-    brand: "Nike Performance",
-    price: {
-      old: "€129,99",
-      current: "€122,49"
-    }
-  };
+  products = products;
 
   changeTest() {
     this.product.name = "sdfsdfsdfsf";
@@ -22,6 +15,9 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.changeTest = this.changeTest.bind(this);
+    this.state = {
+      likedProducts: []
+    }
   }
 
   processProductData(data) {
@@ -31,23 +27,45 @@ class App extends Component {
   }
 
   componentDidMount() {
-    ReactDOM.findDOMNode(this).addEventListener("productLiked", () => {
-      console.log("Product liked");
+    ReactDOM.findDOMNode(this).addEventListener("productLiked", (e) => {
+      const liked = this.state.likedProducts;
+      const currentProduct = JSON.parse(e.detail);
+      liked.push(currentProduct);
+      this.setState({
+        likedProducts: liked
+      });
     });
   }
 
   render() {
+    const productsHtml = this.products.map((product, i) => {
+      return (
+        <div className="product-grid-item" key={i}>
+          <z-product-card product={this.processProductData(product)}></z-product-card>
+        </div>
+      );
+    });
+
+    const likedHtml = this.state.likedProducts.map((product, i) => {
+      return (
+        <li key={i}>
+          <img src={product.image} alt={product.name} />{product.name}
+        </li>
+      );
+    });
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" onClick={this.changeTest} />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      <h1>Product card</h1>
-      <z-product-card product={this.processProductData(this.product)}></z-product-card>
+      <div className="demo-container">
+        <div className="col-left">
+          <h2>Custom web component test</h2>
+            {productsHtml}
+        </div>
+        <div className="col-right">
+          <h2>Liked products</h2>
+          <ul className="liked-products">
+            {likedHtml}
+          </ul>
+        </div>
       </div>
     );
   }
---
+++ src/mocks/products.js
@@ -0,0 +1,65 @@
+export const products = [
+  {
+    name: "RICMANN - Business-Schnürer - cognac",
+    image: "https://mosaic02.ztat.net/vgs/media/catalog-lg/A0/11/2A/02/QO/11/A0112A02Q-O11@12.jpg",
+    brand: "Aldo",
+    price: {
+      old: "€119,99",
+      current: "€112,49"
+    }
+  },
+  {
+    name: "LOVERICIA - Sneaker low - cognac",
+    image: "https://mosaic02.ztat.net/vgs/media/catalog-lg/A0/11/2O/00/BO/11/A0112O00B-O11@12.jpg",
+    brand: "Aldo",
+    price: {
+      old: "€129,99",
+      current: "€122,49"
+    }
+  },
+  {
+    name: "Langarmshirt - black/legion green/reflective orange",
+    image: "https://mosaic02.ztat.net/vgs/media/catalog-lg/A0/11/2O/00/AK/11/A0112O00A-K11@12.jpg",
+    brand: "Nike Performance",
+    price: {
+      old: "€129,99",
+      current: "€122,49"
+    }
+  },
+  {
+    name: "Langarmshirt - black/legion green/reflective orange",
+    image: "https://mosaic02.ztat.net/vgs/media/catalog-lg/A0/11/2O/00/AK/11/A0112O00A-K11@12.jpg",
+    brand: "Nike Performance",
+    price: {
+      old: "€129,99",
+      current: "€122,49"
+    }
+  },
+  {
+    name: "Langarmshirt - black/legion green/reflective orange",
+    image: "https://mosaic02.ztat.net/vgs/media/catalog-lg/A0/11/2O/00/AK/11/A0112O00A-K11@12.jpg",
+    brand: "Nike Performance",
+    price: {
+      old: "€129,99",
+      current: "€122,49"
+    }
+  },
+  {
+    name: "Langarmshirt - black/legion green/reflective orange",
+    image: "https://mosaic02.ztat.net/vgs/media/catalog-lg/A0/11/2O/00/AK/11/A0112O00A-K11@12.jpg",
+    brand: "Nike Performance",
+    price: {
+      old: "€129,99",
+      current: "€122,49"
+    }
+  },
+  {
+    name: "Langarmshirt - black/legion green/reflective orange",
+    image: "https://mosaic02.ztat.net/vgs/media/catalog-lg/A0/11/2O/00/AK/11/A0112O00A-K11@12.jpg",
+    brand: "Nike Performance",
+    price: {
+      old: "€129,99",
+      current: "€122,49"
+    }
+  }
+]
